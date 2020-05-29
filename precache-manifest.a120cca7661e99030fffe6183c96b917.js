c20b8947f0478651df79f4aa3f74a1a5db6ce379
Vladimir Bezobrazov <vovan0503@mail.ru>
2020-05-29 23:45:09 +0300

    Updates

---
+++ precache-manifest.a120cca7661e99030fffe6183c96b917.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a9c9bbeadb1387454a6a3cac358e36",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "c03c605e035c9ae78442",
+    "url": "/Portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9e7374c3c3d3955f953e",
+    "url": "/Portfolio/static/js/2.5e34eba5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.5e34eba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c03c605e035c9ae78442",
+    "url": "/Portfolio/static/js/main.a9288034.chunk.js"
+  },
+  {
+    "revision": "f7114bd3362d877c96cd",
+    "url": "/Portfolio/static/js/runtime-main.5f70be4b.js"
+  },
+  {
+    "revision": "015a664eb853608e4afbdef1b36e23f0",
+    "url": "/Portfolio/static/media/about.015a664e.jpeg"
+  },
+  {
+    "revision": "3b67aa71f0d6087fecc1d23f954f3844",
+    "url": "/Portfolio/static/media/canban.3b67aa71.jpg"
+  },
+  {
+    "revision": "832335587d5da03ad0e2300b889e7452",
+    "url": "/Portfolio/static/media/klient.83233558.jpg"
+  },
+  {
+    "revision": "efb6081c45f9bd158fd307be618a1f62",
+    "url": "/Portfolio/static/media/landing.efb6081c.jpg"
+  }
+]);
